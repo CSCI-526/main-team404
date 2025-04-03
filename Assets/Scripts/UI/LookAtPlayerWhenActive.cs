867e593d55372c119dd3783c12585bf6cade58ee
JulianLi515 <lijulian@usc.edu>
2025-04-02 22:23:44 -0700

    attempt2

--- Assets/Scripts/UI/LookAtPlayerWhenActive.cs
+++ Assets/Scripts/UI/LookAtPlayerWhenActive.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LookAtPlayerWhenActive : MonoBehaviour
 {
     public bool isActive;
-    private SpriteRenderer renderer;
+    private SpriteRenderer spriteRenderer;
     public float targetAlpha;
     public float transitionTime;
     private float currentAlpha;
@@ -13,8 +13,8 @@ public class LookAtPlayerWhenActive : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        renderer = GetComponent<SpriteRenderer>();
-        renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 0);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0);
         currentAlpha = 0f;
         isActive = false;
     }
@@ -25,7 +25,7 @@ public class LookAtPlayerWhenActive : MonoBehaviour
         // Approach target alpha based on whether it is active or not
         float targetAlphaValue = isActive ? targetAlpha : 0f;
         currentAlpha = Mathf.MoveTowards(currentAlpha, targetAlphaValue, (targetAlpha / transitionTime) * Time.deltaTime);
-        renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, currentAlpha);
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, currentAlpha);
 
         if (isActive)
         {
